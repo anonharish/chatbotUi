4aa39f27dc2419299c54ea0167f13f4178bf7b7f
Harish.K <kollatiharish@gmail.com>
2026-03-09 14:32:24 +0530

    updated the imports

--- chatbot/src/pages/ProfileInfoPage.tsx
+++ chatbot/src/pages/ProfileInfoPage.tsx
@@ -1,4 +1,4 @@
-import GlassCutCard from "@/components/pages/GlassCutCard";
+import GlassCutCard from "@/components/pages/GlassCutCardV2";
 import "@/features/agent-profile/agentProfile.css";
 import { ProfileHeader } from "@/features/agent-profile/components/ProfileHeader";
 import { ProfileInfoCards } from "@/features/profile-info/ProfileInfoCards";
