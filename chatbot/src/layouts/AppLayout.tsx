3525bc17de0af63c87f57e1f10b1ebe7d5312aa1
Harish.K <kollatiharish@gmail.com>
2026-03-10 13:14:42 +0530

    formated the doucument

--- chatbot/src/layouts/AppLayout.tsx
+++ chatbot/src/layouts/AppLayout.tsx
@@ -1,12 +1,12 @@
-import { Outlet } from 'react-router'
-import { AuthProvider } from '@/context/AuthContext'
-import { Toaster } from '@/components/ui/sonner'
+import { Outlet } from "react-router";
+import { AuthProvider } from "@/context/AuthContext";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function AppLayout() {
-    return (
-        <AuthProvider>
-            <Outlet />
-            <Toaster />
-        </AuthProvider>
-    )
+  return (
+    <AuthProvider>
+      <Outlet />
+      <Toaster />
+    </AuthProvider>
+  );
 }
